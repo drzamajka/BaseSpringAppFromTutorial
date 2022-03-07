f937c1b87903476efb654628b252d5b514b71765
Rafa <drzamajka999@gmail.com>
2022-03-07 15:10:09 +0100

    projek_final_v1

---
+++ src/main/java/com/example/basespringappfromtutorial/DeveloperRepository.java
@@ -0,0 +1,7 @@
+package com.example.basespringappfromtutorial;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface DeveloperRepository extends CrudRepository<Developer, Long> {
+
+}
